66b5327079832b2f69c03bfa6c2a2cdf6b4d9ebc
Svetsyn <svetsyn@gmail.com>
2020-07-29 15:15:14 +0300

    Added new interface

--- Customer.java
+++ Customer.java
@@ -1,4 +1,13 @@
 package medesata;
 
-public class Customer {
+public abstract class Customer implements Customers {
+
+    private String firstName;
+    private String secondName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+
+    //TODO
+
 }
---
+++ Customers.java
@@ -0,0 +1,12 @@
+package medesata;
+
+public interface Customers {
+    String setFirstName();
+    String setSecondName();
+    String setLastName();
+    String setEmail();
+    String setPhoneNumber();
+
+    //TODO
+
+}
