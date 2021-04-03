f7e75cb511a340ec9572ec550e6645c81850403c
Svetsyn <svetsyn@gmail.com>
2021-04-03 16:59:41 +0300

    Add constants!

--- medesta/src/main/java/com/medesta/constant/Constant.java
+++ medesta/src/main/java/com/medesta/constant/Constant.java
@@ -2,4 +2,13 @@ package com.medesta.constant;
 
 public class Constant {
     public static final String CUSTOMER_CURRENT_PATH = "src/main/resources/files/Customer_current.txt";
+    public static final String ROLE_DESCRIPTION_ADMIN = "This role allow to control and organize every part of application. For example :" +
+            "Create, add, change and delete customers, procedures, products, users and their roles!";
+    public static final String ROLE_DESCRIPTION_RECEPTIONIST = "This role allow to control and organize most part of application. For example :" +
+            "Create, add, change and delete customers, procedures, products!";
+    public static final String ROLE_DESCRIPTION_DOCTOR = "This role allow to control and organize special part of application. For example :" +
+            "Create, add, change and delete customers,special procedures and assign tasks to other people!";
+    public static final String ROLE_DESCRIPTION_THERAPIST = "This role allow to control and organize limited part of application. For example :" +
+            "Create, add, change and delete customers!";
+
 }
--- medesta/src/main/java/com/medesta/model/entity/Role.java
+++ medesta/src/main/java/com/medesta/model/entity/Role.java
@@ -3,21 +3,30 @@ package com.medesta.model.entity;
 import com.medesta.model.enums.RoleName;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "roles")
 public class Role extends BaseEntity {
     private RoleName name;
+    private String description;
 
 
     public Role() {
     }
 
     public Role(RoleName roleName) {
-        this.name=roleName;
+        this.name = roleName;
     }
 
+    @Column(name = "description", columnDefinition = "TEXT")
+    public String getDescription() {
+        return description;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     @Enumerated(EnumType.STRING)
     public RoleName getName() {
